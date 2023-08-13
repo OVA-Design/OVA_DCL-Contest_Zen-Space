9e11e678df8aab1fd80336a7c4dd25b0abdc6927
Parker Cai <parkercai3@gmail.com>
2023-08-13 16:16:37 -0700

    import scene model

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { engine, InputAction, inputSystem, Material, MeshCollider, pointerEventsSystem } from '@dcl/sdk/ecs'
+import { engine, GltfContainer, InputAction, inputSystem, Material, MeshCollider, pointerEventsSystem, Transform } from '@dcl/sdk/ecs'
 import { Color4 } from '@dcl/sdk/math'
-
-
-import { bounceScalingSystem, circularSystem } from './systems'
-
-import { setupUi } from './ui'
-import { BounceScaling, Spinner } from './components'
-import { createCube } from './factory'
-import { waterfallSetup } from './waterfall'
+import { waterSetup } from './waterfall'
 
 
 export function main() {
+  //// Import glb models into scene
+  //#region Import
+
+  const landscapeEntity = engine.addEntity()
+  GltfContainer.create(landscapeEntity, { src: 'models/collider.glb' })
+  Transform.create(landscapeEntity, { position: { x: 16, y: 0, z: 16 } })
+
+  //#endregion
+
 
   // water falls
-  waterfallSetup()
+  waterSetup()
 
 }
--- src/waterfall.ts
+++ src/waterfall.ts
@@ -17,12 +17,15 @@ import {
   import { Quaternion, Vector3, Color4, Color3 } from '@dcl/sdk/math'
   import * as utils from '@dcl-sdk/utils'
 
-export function waterfallSetup() {
+export function waterSetup() {
+    // water fall
+    //#region Import
     //#set plane model for video texture
     let waterfallPlane = engine.addEntity()
     Transform.create(waterfallPlane, {
-      position: Vector3.create(16, 0, 16),
-      scale: Vector3.create(1, 6, 1),
+      position: Vector3.create(14.85, 1, 21.15),
+      rotation: Quaternion.fromEulerDegrees(0, 50, 0),
+      scale: Vector3.create(2, 16, 2),
     })
     MeshRenderer.setPlane(waterfallPlane)  
     // import videos as texture maps
@@ -47,4 +50,41 @@ export function waterfallSetup() {
     //   transparencyMode: MaterialTransparencyMode.MTM_AUTO,
     //   alphaTest:0.5
     })
+    //#endregion
+
+
+    // water pond
+    //#region Import
+    //#set plane model for video texture
+    let waterPondPlane = engine.addEntity()
+    Transform.create(waterPondPlane, {
+      position: Vector3.create(16, 1, 16),
+      rotation: Quaternion.fromEulerDegrees(90, 180, 0),
+      scale: Vector3.create(24, 24, 24),
+    })
+    MeshRenderer.setPlane(waterPondPlane)  
+    // import videos as texture maps
+    VideoPlayer.create(waterPondPlane, {
+      src: 'videos/pond.mp4',
+      playing: true,
+      loop: true,
+      volume: 0.3,
+      playbackRate: 0.6,
+    })
+    const waterPondVideoTexture = Material.Texture.Video({ videoPlayerEntity: waterPondPlane })
+    Material.setPbrMaterial(waterPondPlane, {
+      texture: waterPondVideoTexture,
+      albedoColor: Color4.create(0.6118, 0.8275, 0.8588, 0.5),
+      // alphaTexture: waterPondVideoTexture,
+      emissiveTexture: waterPondVideoTexture, // adding the color channel as emissive texture too, this way only the bright parts were visible
+      emissiveColor: Color3.create(0.6118, 0.8275, 0.8588), // Fresh Water Blue / #9cd3db
+      emissiveIntensity: 1, 
+      bumpTexture: waterPondVideoTexture,
+      roughness: 0.3, // turn on this for more shimmers
+    //   metallic: 0.8,  // turn on this for even more shimmers !
+    //   transparencyMode: MaterialTransparencyMode.MTM_AUTO,
+    //   alphaTest:0.5
+    })
+    //#endregion
+
 }
